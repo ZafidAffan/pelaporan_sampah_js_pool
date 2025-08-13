b40c25f3ea492a1ff860436c5970678cbb314d03
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-08-13 10:38:54 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,     // misalnya: 'mysql-production-9020.up.railway.app'
+  host: process.env.DB_HOST,     
   port: 3306,                    // ini penting kalo gak default (opsional gasih)
-  user: process.env.DB_USER,     // root
-  password: process.env.DB_PASS, // JHlxEzHlzdYVbtESyKfeZZDhcvkJbRfW
-  database: process.env.DB_NAME  // railway
+  user: process.env.DB_USER,     
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME  
 });
 
 db.connect(err => {
