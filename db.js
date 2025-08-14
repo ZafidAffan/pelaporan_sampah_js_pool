f865a449aa249218242ef7a077069fffda320f2d
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-08-14 20:56:19 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,     
-  port: 3306,                    // ini penting kalo gak default (opsional gasih)
+  port: 31481,                    // ini penting kalo gak default (opsional gasih)
   user: process.env.DB_USER,     
   password: process.env.DB_PASS, 
   database: process.env.DB_NAME  
