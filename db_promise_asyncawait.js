7cc19add1b30f9a712230cbe34f7a001ecf62223
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-09-06 22:18:32 +0700

    Update db_promise_asyncawait.js

--- db_promise_asyncawait.js
+++ db_promise_asyncawait.js
@@ -1,16 +1,17 @@
-
-// db_promise.js
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: 31481,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 31481,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
 
-module.exports = pool.promise();
+// versi promise pool (bisa async/await)
+const promisePool = pool.promise();
+
+module.exports = promisePool;
