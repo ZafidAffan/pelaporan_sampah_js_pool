1a1f2f0d5c6ff010c2958b6241ccd40734e73afa
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-09-06 22:07:18 +0700

    Update db_promise(asyn_await).js

--- db_promise(asyn_await).js
+++ db_promise(asyn_await).js
@@ -1,1 +1,16 @@
 
+// db_promise.js
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "containers-us-west-45.railway.app",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "pelaporan_sampah",
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
