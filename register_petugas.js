b9ae0203b5558955d589724098914c622d93adff
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-09-23 13:56:46 +0700

    Update register_petugas.js

--- register_petugas.js
+++ register_petugas.js
@@ -9,8 +9,9 @@ router.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   if (req.method === "OPTIONS") return res.sendStatus(200);
-  nex
-  
+  next(); // ✅ perbaikan di sini
+});
+
 // Endpoint register petugas
 router.post("/", async (req, res) => {
   try {
